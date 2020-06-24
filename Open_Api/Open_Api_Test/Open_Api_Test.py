a7dac819f1af1f564f836d1f47ee2905287e416a
Jun <toqur7000@naver.com>
2020-06-24 11:25:40 +0900

    open_api test

--- Open_Api/Open_Api_Test/Open_Api_Test.py
+++ Open_Api/Open_Api_Test/Open_Api_Test.py
@@ -8,16 +8,16 @@ client_secret = "YOUR_CLIENT_SECRET"
 # url = "https://openapi.naver.com/v1/vision/face" # 얼굴감지
 url = "https://openapi.naver.com/v1/vision/celebrity" # 유명인 얼굴인식
 
-file_name = 'ex_01.jpg' #
-#file_name = 'ex_02.jpg'
+#file_name = 'ex_01.jpg' #
+file_name = 'ex_02.jpg'
 
-files = {'image': open('file_name.jpg', 'rb')}
+files = {'image': open(file_name, 'rb')}
 
 headers = {'X-Naver-Client-Id': client_id, 'X-Naver-Client-Secret': client_secret }
 response = requests.post(url,  files=files, headers=headers)
 rescode = response.status_code
 
 if(rescode==200): # 웹 요청이 성공할 경우
-    print (response.text)
+    print(response.text)
 else: # 웹 요청이 실패할 경우
-    print("Error Code:" + rescode)
+    print("Error Code:", rescode)
